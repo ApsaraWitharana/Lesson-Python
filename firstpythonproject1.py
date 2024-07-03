c794a724c49e3a45fda9688245f6778e00d5181d
ApsaraWitharana <asachini095@gmail.com>
2024-07-03 20:55:24 +0530

    implement my 1st task coll print and if/else

---
+++ firstpythonproject1.py
@@ -0,0 +1,23 @@
+
+ # Task 01 ==> print
+myfloat = 7.0
+print(myfloat)
+myfloat = float(7)
+print(myfloat)
+
+
+print("Starting the program")
+
+def my_function():
+
+  a, b = 3, 4
+  if a > b:
+   print("a is greater than b")
+  else:
+   print("b is greater than or equal to a")
+
+my_function()
+
+
+
+
