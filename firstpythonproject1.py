5799d9fbf9ed5f39e4913f64032e6727b19e6bad
ApsaraWitharana <asachini095@gmail.com>
2024-07-08 23:38:53 +0530

    update-Ex-Basic Operators

--- firstpythonproject1.py
+++ firstpythonproject1.py
@@ -143,3 +143,41 @@ all_numbers = odd_numbers + even_numbers
 print(all_numbers)
 
 print([1,2,3] * 3)
+
+# Ex-01
+
+x = object()
+y = object()
+
+# TODO: change this code
+x_list = [x] * 10
+y_list = [y] * 10
+big_list = x_list + y_list
+
+print("x_list contains %d objects" % len(x_list))
+print("y_list contains %d objects" % len(y_list))
+print("big_list contains %d objects" % len(big_list))
+
+print("==============")
+
+# testing code
+if x_list.count(x) == 10 and y_list.count(y) == 10:
+    print("Almost there...")
+if big_list.count(x) == 10 and big_list.count(y) == 10:
+     print("Sorry!!!!")
+
+print("==============")
+
+# Ex-02
+
+for x in range(10):
+ print("Sorry!!!!:",x)
+
+print("==============")
+
+# Ex-03
+
+for strings in range(10):
+ print("Good Night!!")
+
+print("==============")
