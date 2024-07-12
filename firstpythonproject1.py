231073a806c20b325f69569887a3c9329524217b
ApsaraWitharana <asachini095@gmail.com>
2024-07-13 00:05:57 +0530

    update-String Formatting

--- firstpythonproject1.py
+++ firstpythonproject1.py
@@ -203,5 +203,41 @@ for strings in range(10):
 
  print("==============")
 
+ # Day 4 ---> String Formatting
+
+ # This prints out "Hello, John!"
+ name = "John"
+ print("Hello, %s!" % name)
+
+ print("==============")
+
+ # This prints out "John is 23 years old."
+ name = "Done"
+ age = 23
+ print("%s is %d years old." % (name, age))
+
+ print("==============")
+ # Here are some basic argument specifiers you should know:
+ #
+ # %s - String (or any object with a string representation, like numbers)
+ #
+ # %d - Integers
+ #
+ # %f - Floating point numbers
+ #
+ # %.<number of digits>f - Floating point numbers with a fixed amount of digits to the right of the dot.
+ #
+ # %x/%X - Integers in hex representation (lowercase/uppercase)
+
+
+ # Exercise
+
+ data = ("Done", "The", 53.44)
+ format_string = "Hello %s %s. Your current balance is Rs%s."
+
+ print(format_string % data)
+
+
+
 
 
