7c8725d3775b21e7f08c90735412b767d17c3637
ApsaraWitharana <asachini095@gmail.com>
2024-07-16 00:21:00 +0530

    updatate-Loops/while/gm.bs

--- firstpythonproject1.py
+++ firstpythonproject1.py
@@ -406,4 +406,13 @@ for x in range(3, 6):
 
 # Prints out 3,5,7
 for x in range(3, 8, 2):
-     print(x)
+    print(x)
+
+print("==============")
+
+# "while" loops
+
+count = 0
+while count < 5:
+     print(count)
+     count += 1  # This is the same as count = count + 1
