da9fc1eb96fc16e908d8d5cac750f8b466d3fa85
ApsaraWitharana <asachini095@gmail.com>
2024-07-16 00:17:41 +0530

    updatate-Loops/for-loop

--- firstpythonproject1.py
+++ firstpythonproject1.py
@@ -382,3 +382,28 @@ if name in ["Dusvi","Hero"]:
 
  if not second_number:
      print("6")
+
+
+print("==============")
+
+#Day 6 ---> Loops
+
+ # The "for" loop ->> For loops iterate over a given sequence.
+
+primes = [2, 3, 5, 7]
+for prime in primes:
+ print(prime)
+
+print("==============")
+
+# Prints out the numbers 0,1,2,3,4
+for x in range(5):
+     print(x)
+
+# Prints out 3,4,5
+for x in range(3, 6):
+     print(x)
+
+# Prints out 3,5,7
+for x in range(3, 8, 2):
+     print(x)
